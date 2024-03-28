74d689a345328be2f4e080b59b37c20d5ba03654
Danylo Varfolomieiev <danylo.varfolomieiev@userway.org>
2024-03-28 14:29:08 +0200

    upgrade tsconfig

--- cypress/config/cypress.config.js
+++ cypress/config/cypress.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     screenshotOnRunFailure: false,
     baseUrl: 'http://localhost:5173',
     defaultCommandTimeout: 10000
-
-
   }
 });
