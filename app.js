66c814178efa0b2955239051937bf1a4efbd3239
danylovar <152491830+danylovar@users.noreply.github.com>
2024-03-29 15:43:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 app.use(express.static(__dirname + '/src'));
 const port = 5173;
 app.listen(port);
-console.log('server on: ' + port);
+console.log('server on http://localhost:' + port);
