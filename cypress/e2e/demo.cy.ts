cf80a38d4609e32ddcecb0dc92b0ae97d39585c1
Oleksandr Zalizniak <alex@userway.org>
2024-05-05 10:59:59 -0400

    Added more tests

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -1,3 +1,17 @@
+import 'cypress-each'
+
+const URLS = [
+  'article.html',
+  'index.html',
+  'privacy.html',
+  'terms.html',
+];
+
+it.each(URLS)('%s page', (page) => {
+  console.log(`Testing page: ${page}`);
+  cy.visit(page);
+});
+
 it('should have the right text in the headline', () => {
   cy.visit('/');
 
