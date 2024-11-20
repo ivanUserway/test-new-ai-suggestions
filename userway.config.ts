368165401d195e996da16b4eb57fd87c5a4269d0
Ivan Haliurov <ivan.haliurov@userway.org>
2024-11-20 16:12:04 +0200

    qa api

--- userway.config.ts
+++ userway.config.ts
@@ -3,5 +3,6 @@ export default {
   organization: "ivan-haliurov-userway-org",
   project: "test-website-1-mirko-github",
   reportPaths: ["./uw-a11y-reports"],
+  server: "https://api.qa.userway.dev",
   token: "2zggtpbjjg9iwncgrkqe7ljmbo3gnor74zk9nn64",
 } satisfies Config;
