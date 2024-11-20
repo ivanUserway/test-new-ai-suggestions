dc7c2f0ba215e3d6c9caa8e5413befab4d2a71bb
Ivan Haliurov <ivan.haliurov@userway.org>
2024-11-20 14:51:29 +0200

    add new token

--- userway.config.ts
+++ userway.config.ts
@@ -4,5 +4,5 @@ export default {
   project: "test-website-1-mirko-github",
   reportPaths: ["./uw-a11y-reports"],
   server: "https://api.qa.userway.dev",
-  token: "2zggtpbjjg9iwncgrkqe7ljmbo3gnor74zk9nn64",
+  token: "qnzrdzkyrurkppys7knx6rvadgmb5uixkqxiu4b5",
 } satisfies Config;
