7ec69f6845c77515d45de53dc59e6c0fecff220e
Ivan Haliurov <ivan.haliurov@userway.org>
2025-01-09 17:06:02 +0200

    dev env

--- userway.config.ts
+++ userway.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@userway/cicd-cli";
 export default {
   organization: "ivan-haliurov-userway-org",
   project: "test-website-1-mirko-github",
-  token: "qnzrdzkyrurkppys7knx6rvadgmb5uixkqxiu4b5",
+  token: "2tanu7mqh1f6djcmwbemfoiewz5rrbptmigj0uc8",
+  server: "https://cicd.dev.userway.dev", //change
   reportPaths: ["./uw-a11y-reports"],
 } satisfies Config;
