f5fec0a7720513c68581fa3a234b996f31638d3e
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2025-05-27 12:47:26 +0300

    update package

--- userway.config.ts
+++ userway.config.ts
@@ -3,6 +3,6 @@ export default {
   organization: "ivan-haliurov-levelaccess-com-hspeq",
   project: "test-mirko",
   token: "dppsihfp0rcjciezyraee26lqm4ccuk3msitnxfx",
-  server: "https://cicd.dev.userway.dev", //change
+  server: "https://api.dev.userway.dev", //change
   reportPaths: ["./uw-a11y-reports"],
 } satisfies Config;
