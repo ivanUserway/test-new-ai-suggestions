6d264c04e3a56dc13e0bafbb10f4a9d3984586d6
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2025-05-27 12:31:58 +0300

    set new project

--- userway.config.ts
+++ userway.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "@userway/cicd-cli";
 export default {
-  organization: "ivan-haliurov-1-userway-org",
+  organization: "ivan-haliurov-levelaccess-com-hspeq",
   project: "test-mirko",
-  token: "4gsntt05mghsdggqxr4glmporuhx3ig5p70j7lne",
+  token: "dppsihfp0rcjciezyraee26lqm4ccuk3msitnxfx",
   server: "https://cicd.dev.userway.dev", //change
   reportPaths: ["./uw-a11y-reports"],
 } satisfies Config;
