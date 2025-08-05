e9efb9fe49511d4873418d0b5a92f64d7bdbb4ef
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2025-08-05 11:32:16 +0300

    update setup

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "@userway/cicd-cli";
 export default {
   organization: "ivan-haliurov-levelaccess-com-hspeq",
-  project: "test-mirko",
-  token: "eikw2j6pj80dnlupfqi0fwupccmjwvxpacrrpv0q",
+  project: "test-new-ai-suggestions",
+  token: "k7czoggbfwxpf4j3hbaw7syg45i2zmrx43m4ziwz",
   server: "https://api.dev.userway.dev", //change
   reportPaths: ["./uw-a11y-reports"],
 } satisfies Config;
