daacb06588b8a71a9ef24887b7098a9424e64de3
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2025-08-05 11:18:16 +0300

    test new ai suggestions

--- userway.config.ts
+++ userway.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "@userway/cicd-cli";
 export default {
   organization: "ivan-haliurov-levelaccess-com-hspeq",
   project: "test-mirko",
-  token: "dppsihfp0rcjciezyraee26lqm4ccuk3msitnxfx",
+  token: "eikw2j6pj80dnlupfqi0fwupccmjwvxpacrrpv0q",
   server: "https://api.dev.userway.dev", //change
   reportPaths: ["./uw-a11y-reports"],
 } satisfies Config;
