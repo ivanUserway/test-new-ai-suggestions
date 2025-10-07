1f78e7bb6170ddb04fca99933bf02858505d76c5
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2025-10-07 15:47:20 +0300

    upd project

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@level-ci/cli";
 export default {
   organization: "ivan-haliurov-levelaccess-com-hspeq",
-  project: "test-new-ai-suggestions",
+  project: process.env.LEVEL_CI_TOKEN,
   token: "9oaxiislzdxdfwh5dyt7txvvddxdtqinr2huers4",
   server: "https://api.dev.userway.dev", //change
   reportPaths: ["./level-ci-reports"],
--- playwright/e2e/demo.spec.ts
+++ playwright/e2e/demo.spec.ts
@@ -1,18 +1,9 @@
 import { test, expect } from "@playwright/test";
-import { setupUserway, userwayAnalysis } from "@userway/a11y-playwright";
+import { levelAnalyze } from "@level-ci/a11y-playwright";
 
 test("should have the right text in the headline", async ({ page }) => {
   await page.goto("/");
 
   await expect(page.locator("#logoLink")).toBeVisible();
-  setupUserway({
-    issueScreenshots: true,
-    detailedScreenshots: true,
-    printViolationsTable: false,
-  });
-  await userwayAnalysis(page, {
-    includeBestPractices: true,
-    switchOff: false,
-    strict: false,
-  });
+  await levelAnalyze(page);
 });
